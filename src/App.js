aed411202962623c198abbd8d4cc026fc64295b2
Nihat <abdullazadenihat@gmail.com>
2022-12-11 00:23:30 +0400

    Controlled Input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import ShowHide from './tutorial/3-conditional-rendering/setup/3-show-hide'
-
+import ControlledInputs from './tutorial/4-forms/setup/1-controlled-inputs'
 
 function App() {
   return (
     <div className='container'>
       <h2>Advanced Tutorial</h2>
-      <ShowHide> </ShowHide>
+      <ControlledInputs> </ControlledInputs>
     </div>
   )
 }
--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,32 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+
+  const formHandler = (e) => {
+    e.preventDefault();
+    console.log(name, )
+  }
+
+  return (
+    <>
+      <form onSubmit={(e) => { formHandler(e) }}>
+        <div className='form-group'>
+          <label htmlFor=''> Name </label>
+          <input type='text' value={name} onChange={(e) => { setName(e.target.value) }} ></input>
+        </div>
+        <div className='form-group'>
+          <label htmlFor=''> Email </label>
+          <input type='text' value={email} onChange={(e) => { setEmail(e.target.value) }} ></input>
+        </div>
+        <div className='form-group'>
+          <button type='submit'>  Send it   </button>
+        </div>
+      </form>
+    </>
+  )
 };
 
 export default ControlledInputs;
