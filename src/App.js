45ada1912c5f109d787486d0ca671bd21475abb2
unknown <abdullazadeniht@gmail.com>
2022-12-11 15:17:19 +0400

    props drilling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import UseRefBasics from './tutorial/5-useRef/setup/1-useRef-basics'
+import PropDrilling from './tutorial/7-prop-drilling/setup/1-prop-drilling'
+
 function App() {
   return (
     <div className='container'>
       <h2>Advanced Tutorial</h2>
-      <UseRefBasics> </UseRefBasics>
+      <PropDrilling> </PropDrilling>
     </div>
   )
 }
--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -1,10 +1,45 @@
 import React, { useState } from 'react';
-
+import { data } from '../../../data';
 // more components
 // fix - context api, redux (for more complex cases)
 
+
+
 const PropDrilling = () => {
-  return <h2>prop drilling</h2>;
+
+  const [people, setPeople] = useState(data)
+  return (
+    <>
+      <h3> Prop Drilling </h3>
+      <div>
+        <PeopleList people={people} ></PeopleList>
+      </div>
+    </>
+  )
 };
+
+
+const PeopleList = ({ people }) => {
+  return (
+    <>
+      {people.map(p => {
+        return (
+          <div key={p.id}>
+            <PeopleSingle data={p}>  </PeopleSingle>
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+const PeopleSingle = (props) => {
+  return (
+    <div >
+      <span> {props.data.id}  </span> <span>{props.data.name}  </span>
+    </div>
+  )
+}
+
 
 export default PropDrilling;
